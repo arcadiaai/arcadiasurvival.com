b92a4bc798007f2dd7c8b1cef6facbba22f99bdf
Chad <chad@coopscommo.com>
2026-05-05 17:44:37 +0000

    Brand foundation: tokens, fonts, layouts, header/footer

---
+++ src/env.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="astro/client" />
+
+declare module '@fontsource-variable/crimson-pro';
+declare module '@fontsource-variable/inter';
+declare module '@fontsource/ibm-plex-mono/400.css';
+declare module '@fontsource/ibm-plex-mono/500.css';
